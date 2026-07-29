7f13c626ee7f81e67750ebb536de8433b689a54b
gulammohammad07 <gulammohd8080@gmail.com>
2026-07-30 01:58:48 +0530

    Remove env file from git tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
